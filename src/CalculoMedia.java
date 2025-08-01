de14c6befa6728e531557b80979cc6a09e535f1f
washryan <washr.3103@gmail.com>
2025-08-01 15:37:52 -0300

    feat: exercício cálculo de média

---
+++ src/CalculoMedia.java
@@ -0,0 +1,12 @@
+public class CalculoMedia {
+    public static void main(String[] args) {
+        double nota1 = 8.5;
+        double nota2 = 7.0;
+        double nota3 = 9.0;
+        double nota4 = 6.5;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("Media das notas: " + media);
+    }
+}
